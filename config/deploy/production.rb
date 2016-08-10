c49c7dc72de8ded7cf39d97170e20b20901c5af3
Xingyu Ye <xingyu.ye@outlook.com>
2016-08-10 13:16:31 +0930

    modify deploy/producton.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,15 +2,15 @@ set :branch,      'master'
 set :rails_env,   'production'
 set :migrate_env, 'production'
 
-set :server_address, "www.wilnuoter_wine_rails.com.au"
+set :server_address, "43.229.63.71"
 
-server "www.wilnuoter_wine_rails.com.au", :app, :web, :db, :primary => true
+server "43.229.63.71", :app, :web, :db, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end  
   task :stop do ; end  
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd '/home/deploy/wilnuoter_wine_rails/current' ; bundle exec rake db:migrate RAILS_ENV=production"
+    run "cd '/home/deploy/wilnuoter/current' ; bundle exec rake db:migrate RAILS_ENV=production"
   end
 end
