387b39e7ef9866f06409e14d6ce354b120ac14a8
sunny zhou <webztj@gmail.com>
2016-08-16 14:25:30 +0930

    migrate blog model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816035632) do
+ActiveRecord::Schema.define(version: 20160816043827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,20 @@ ActiveRecord::Schema.define(version: 20160816035632) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "blogs", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.string   "url_name"
+    t.string   "feature_image_file_name"
+    t.string   "feature_image_content_type"
+    t.integer  "feature_image_file_size"
+    t.datetime "feature_image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogs", ["url_name"], name: "index_blogs_on_url_name", using: :btree
 
   create_table "home_sliders", force: true do |t|
     t.string   "name"
