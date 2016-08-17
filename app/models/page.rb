d136093a477d75b387f4c4a3496ca520b4ccbeb3
sunny zhou <webztj@gmail.com>
2016-08-17 23:22:51 +0930

    fix page issue

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,6 @@ class Page < ActiveRecord::Base
   belongs_to :parent, class_name: 'Page'
   has_many :children, class_name: 'Page', foreign_key: 'parent_id'
 
-  attr_accessible :body, :name, :url_name, :banner_image, :parent_id, :custom_url
   has_attached_file :banner_image, styles: {small: '384x100#', large: '1920x500#'}
 
   validates :name, presence: true
