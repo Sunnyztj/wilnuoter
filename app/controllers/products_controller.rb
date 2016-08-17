029b0d68ce464db42c33e54b769166bc3b78c24a
Xingyu Ye <xingyu.ye@outlook.com>
2016-08-17 14:42:37 +0930

    finish wine list page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,13 @@ class ProductsController < ApplicationController
   def wine_list
     @resp = Hash.new
 
-    products = Product.drafted
+    product_count_per_page = 12
+    page = params[:page] == nil ? 1 : params[:page]
+
+    products = Product.drafted.page(page).per(product_count_per_page)
 
     @resp[:products] = products
+    @resp[:current_page] = page
+    @resp[:product_count_per_page] = product_count_per_page
   end
 end
