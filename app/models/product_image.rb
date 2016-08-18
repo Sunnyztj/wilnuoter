6ea8a8dd93d9a520418be83b8f0f5d64abcd8008
Xingyu Ye <xingyu.ye@outlook.com>
2016-08-18 13:03:33 +0930

    refine product show page

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,6 +1,6 @@
 class ProductImage < ActiveRecord::Base
   belongs_to :product
 
-  has_attached_file :image, styles: {small: '300x225#', large: '800'}
+  has_attached_file :image, styles: {small: '300x225#', middle: '500x500#', large: '800'}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
