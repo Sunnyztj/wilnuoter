05d3feea4332f82ec8282ebdfbb857b2e79c7d13
Xingyu Ye <xingyu.ye@outlook.com>
2016-08-31 11:07:31 +0930

    fix locale bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def set_admin_locale
+  before_filter :set_locale
+
+  def set_locale
     I18n.locale = 'zh-CN'
   end
 end
